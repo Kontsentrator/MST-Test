9e3b0d6b573cd9e1ef357c41fc3e7a6c629827eb
Kontsentrator <dima.dima-73@yandex.ru>
2021-08-14 10:59:43 +0300

    Create script.js

---
+++ js/script.js
@@ -0,0 +1,7 @@
+var slider_img_list = $(".slider__img");
+var slider_img_active = $(".slider__img_active");
+
+$(document).ready(function() {
+    //slider_img_active.removeClass("slider__img_active");
+    //slider_img_list.eq(1).addClass("slider__img_active");
+});
