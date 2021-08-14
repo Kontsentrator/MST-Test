c5acd9d4fbd3a3c8cecbb1788f3816836bfe04b7
Kontsentrator <dima.dima-73@yandex.ru>
2021-08-14 11:55:30 +0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,7 +1,18 @@
 var slider_img_list = $(".slider__img");
+var aside_links_list = $(".aside-nav__link");
+
 var slider_img_active = $(".slider__img_active");
+var aside_link_active = $(".aside-nav__link_active");
 
 $(document).ready(function() {
-    //slider_img_active.removeClass("slider__img_active");
-    //slider_img_list.eq(1).addClass("slider__img_active");
+    $(".aside-nav__list").on('click', ".aside-nav__item", function() {
+        aside_link_active.removeClass("aside-nav__link_active");
+        $(this).children().addClass("aside-nav__link_active");
+        aside_link_active = $(".aside-nav__link_active");
+        var id = $(this).index();
+
+        slider_img_active.removeClass("slider__img_active");
+        slider_img_list.eq(id).addClass("slider__img_active");
+        slider_img_active = $(".slider__img_active");
+    });
 });
